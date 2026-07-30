a06db0270f5e21fcf68480584a73f91ee9bb843e
AnhLDP <leducphananh@gmail.com>
2026-07-30 17:00:59 +0700

    chore: configure dev server port, clear database password, and set MapStruct unmapped target policy to ignore

--- demo/src/main/java/com/example/demo/mapper/ProductMapper.java
+++ demo/src/main/java/com/example/demo/mapper/ProductMapper.java
@@ -2,12 +2,13 @@ package com.example.demo.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 
 import com.example.demo.dto.request.ProductRequest;
 import com.example.demo.dto.response.ProductResponse;
 import com.example.demo.entity.Product;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProductMapper {
     Product toEntity(ProductRequest request);
 
